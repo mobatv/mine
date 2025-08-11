b05a6275a34230bba6667a3e4c77b4bc2ee6b92e
mobatv <53135012+mobatv@users.noreply.github.com>
2025-08-11 10:33:09 -0700

    Add files via upload

---
+++ run_miner.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# For more usage info: hellminer --help
+
+# Example SSL URL
+# ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+
+./hellminer -c stratum+tcp://na.luckpool.net:3960 -u RQxidG5ybruLPiKxnzoeskFHGd7kkNUD4a.WORKER -p x --cpu 7
